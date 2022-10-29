1395280c3d2a7631587044940e2aa6a1dba2d084
abin7-alpha <paulabin9@gmail.com>
2022-10-29 18:25:36 +0530

    Day 1

--- day1.py
+++ day1.py
@@ -1,3 +1,15 @@
+#A: 607 (N/A - no previous sum)
+#B: 618 (increased)
+#C: 618 (no change)
+#D: 617 (decreased)
+#E: 647 (increased)
+#F: 716 (increased)
+#G: 769 (increased)
+#H: 792 (increased)
+#In this example, there are 5 sums that are larger than the previous sum.
+#
+#Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+
 def cal_pre_values(array):
     total_pre_values = 0
     count = 0
