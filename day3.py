62f2645668cc9c328ed3d8e2ecea10dd5b4569bb
abin7-alpha <paulabin9@gmail.com>
2022-10-31 03:42:06 +0530

    Day 3 part1

---
+++ day3.py
@@ -0,0 +1,78 @@
+def calc_bits(number_list):
+    zeroes = number_list.count(0)
+    ones = number_list.count(1)
+
+    return zeroes, ones
+
+def empty_lists_needed(all_number_list):
+    for row in all_number_list:
+        each_row = list(row)
+        return len(each_row)
+
+def convert_columns(all_number_list):
+    seperated_list = [[] for x in range(empty_lists_needed(all_number_list))]
+    count = 0
+    for row in all_number_list:
+        each_row = list(row)
+        for number in each_row:
+            seperated_list[count].append(int(number))
+            if count == len(each_row)-1:
+                count = 0
+            else:
+                count += 1
+    return seperated_list
+
+def calc_values(all_number_list):
+    count = 0
+    converted_data = convert_columns(all_number_list)
+    metric = {}
+    for item in converted_data:
+        a, b = calc_bits(item)
+        metric[f'bit{str(count)}'] = [a, b]
+        count += 1
+    return metric
+
+def final_binary(all_number_list):
+    binary_number = ""
+    calculated_metric = calc_values(all_number_list)
+    for key,value in calculated_metric.items():
+        if value[0] < value[1]:
+            binary_number += "0"
+        else:
+            binary_number += "1"
+
+    return binary_number
+
+def least_binary(all_number_list):
+    binary = final_binary(all_number_list)
+    l_binary = ""
+    for number in list(binary):
+        if number == "1":
+            l_binary += "0"
+        else:
+            l_binary += "1"
+
+    return int(binary), int(l_binary)
+
+def binary_to_decimal(n):
+    decimal = 0
+    power = 1
+    while n>0:
+        rem = n%10
+        n = n//10
+        decimal += rem*power
+        power = power*2
+        
+    return decimal
+
+files = open("day3.txt", "r")
+data = []
+for file in files:
+    data.append(file.split("\n")[0])
+
+binary1, binary2 = least_binary(data)
+
+by_product = binary_to_decimal(binary1) * binary_to_decimal(binary2)
+print(by_product)
+
+        
